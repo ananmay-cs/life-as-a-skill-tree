34c7bb83f20179ce34ed85258a84a1ec3955fdf1
Ananmay <90300277+ken-me@users.noreply.github.com>
2026-06-14 11:12:22 +0530

    first commit

---
+++ skill_trees.py
@@ -0,0 +1,28 @@
+SKILL_TREES = {
+    "AI Engineer": [
+        "Python",
+        "Git",
+        "Machine Learning",
+        "Deep Learning",
+        "Model Deployment",
+        "AI Engineer"
+    ],
+
+    "Web Developer": [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Backend",
+        "Full Stack Developer"
+    ],
+
+    "Data Scientist": [
+        "Python",
+        "Statistics",
+        "Pandas",
+        "Machine Learning",
+        "Data Visualization",
+        "Data Scientist"
+    ]
+}
